26a0cf86c1ca5df5c9d16eff62fd599e6895156b
Deniz Yalim <denizyalimyilmaz@gmail.com>
2025-12-30 22:34:13 +0300

    a

--- main.py
+++ main.py
@@ -7,17 +7,28 @@ SIM_DAY_COUNT = 1000
 AVG_PRICE_DIFF = 1
 AVG_PROFIT = 1
 FORECASTER_CORRECT_CHANCE = 70
+ 
+
 
 
 def test():
+    df = pd.read_csv("AAPL_last_1000_ohlc.csv")
+
     forecaster = forecasterAlgorithm(FORECASTER_CORRECT_CHANCE)
     tokenizer = Tokenizer(AVG_PRICE_DIFF)
-    for _ in range(SIM_DAY_COUNT):
+    
+    
+    for day in range(SIM_DAY_COUNT):
         
         # tokenizer'a market low'u gönderilecek; kar içinde market_close
 
         isGuessCorrect = forecaster.guess()
-        tokenizer.eod(isGuessCorrect, AVG_PROFIT)
+
+        if isGuessCorrect:
+            tokenizer.eod(isGuessCorrect, df["Close"] - df["Open"])
+
+        else:
+            tokenizer.eod(isGuessCorrect, df["Open"] - df["Low"])    
     
     # print("kumbara: ", tokenizer.getKumbara())
     print(tokenizer.getProfit())
